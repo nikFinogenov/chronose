d9e7e2136a11533cbf27089a1f02d0141b144ea6
Nikita Finogenov <nikita.finogenov@gmail.com>
2025-03-28 00:08:11 +0100

    mobile fix

--- client/src/pages/Year.jsx
+++ client/src/pages/Year.jsx
@@ -11,7 +11,7 @@ const Year = observer(() => {
       <div className="m-5 w-full">
         <div className="flex flex-col flex-1 items-center">
           <h1 className="text-3xl mb-5">{new Date(dateStore.currentDate).getFullYear()}</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 lg gap-5 justify-center">
             {Array.from({ length: 12 }, (_, i) => (
               <MicroMonth key={i + 1} month={(i + 1).toString().padStart(2, "0")} />
             ))}
